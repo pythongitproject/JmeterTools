bde0b01c01ed481bc05c647328bbc95c083c8753
linweili <295287765@qq.com>
2018-07-03 18:02:13 +0800

    2

--- src/test/java/Testdsf.java
+++ src/test/java/Testdsf.java
@@ -1,36 +1,47 @@
 import com.google.protobuf.Internal;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import td.functions.UserName;
 
+import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by linweili on 2018/6/14/0014.
  */
 public class Testdsf {
-    public static void main(String[] args){
-        String str = "123456"+"123456"+"1528960439497";
-        String sign = DigestUtils.md5Hex("1234561234561528960624172");
-        System.out.println(sign);
-        //993575f69932bc2d2a8e22d16c1bd29d
-        System.out.println(new Random().nextInt(2));
-        String bankCard = "955880120291";
-        String cardCode1 = null;
-        cardCode1 = new Random().nextInt(899999) + 1000000 + "";
-        bankCard = bankCard + cardCode1;
-        System.out.println(bankCard);
-        String[] telFirst="19922,16633".split(",");
-        String first=telFirst[new Random().nextInt(2)];
-        String second= new Random().nextInt(89999)+100000+"";
 
-        System.out.println("telno:"+first+second);
-//        for (int i=1;i<100;i++){
-            String[] end = {"@163.com","@qq.com","@tuandai.com"};
-            String start = "ghabqcdefijhtrshsdfsfaarehndfauyksrtertwqazweerfef".substring((int)(Math.random()*5),(int)(Math.random()*5+15))+new Random().nextInt(10000)+new Random().nextInt(100);
-            String email = start + end[new Random().nextInt(3)];
-            System.out.println("email:"+email);
-//        }
-        System.out.println(new UserName().getXing());
+    private static final Logger logger = LoggerFactory.getLogger(Testdsf.class);
+    private static final String  QUEUE_NAME = "jmeter_queue";
 
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置RabbitMQ相关信息
+        factory.setHost("node.td-k8s.com");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setPort(5672);
+        //创建一个新的连接
+        Connection connection = factory.newConnection();
+        logger.info("连接mq:"+"node.td-k8s.com");
+        //创建一个通道
+        Channel channel = connection.createChannel();
+        //声明一个队列
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        //发送消息到队列中
+        String message = "Hello MQ";
+        channel.basicPublish("",QUEUE_NAME,null,message.getBytes("UTF-8"));
+        logger.info("推送消息:"+message);
+        System.out.println("producer send:" + message);
+        //关闭通道及连接
+        channel.close();
+        connection.close();
+        logger.info("mq已断开:"+message);
     }
 }
