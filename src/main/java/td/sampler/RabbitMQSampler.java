60c509aac4342ea21d13bf19ed9ae8e60ec44866
linweili <295287765@qq.com>
2018-07-09 19:10:29 +0800

    rabbitmq

--- src/main/java/td/sampler/RabbitMQSampler.java
+++ src/main/java/td/sampler/RabbitMQSampler.java
@@ -1,7 +1,5 @@
 package td.sampler;
 
-import com.drew.metadata.exif.PanasonicRawIFD0Descriptor;
-import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -11,10 +9,7 @@ import org.apache.jmeter.samplers.SampleResult;
 import org.apache.jmeter.testelement.TestStateListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -29,13 +24,13 @@ public class RabbitMQSampler extends AbstractSampler implements TestStateListene
      * 基本参数
      */
     private static final String Host = "node.td-k8s.com";
-    private static final int Port = 5672;
+    private static final String Port = "5672";
     private static final String VirtualHost = "/globalBizEvent";
     private static final String Username = "admin";
     private static final String Password = "admin";
-    private static final String  TYPE = "exchange";
-    private static final String  TYPE_VALUE = "userRegisterExchange";
-    private static final String MESSAGE = "message";
+    private static final String ExchangeType = "fanout";
+    private static final String ExchangeName = "userRegisterExchange";
+    private static final String Message = "message";
 
 
 
@@ -54,7 +49,7 @@ public class RabbitMQSampler extends AbstractSampler implements TestStateListene
 
             //广播
             Boolean status = Broadcast();
-            System.out.println("广播结果："+status);
+            System.out.println("MQ广播信息结果："+status);
 
             result.sampleEnd();
             result.setSuccessful(true);
@@ -73,72 +68,144 @@ public class RabbitMQSampler extends AbstractSampler implements TestStateListene
         return result;
     }
 
+    /**
+     * 广播消息
+     * @return
+     * @throws IOException
+     * @throws TimeoutException
+     */
     private Boolean Broadcast() throws IOException, TimeoutException {
         //创建连接工厂
         ConnectionFactory factory = new ConnectionFactory();
-        //设置RabbitMQ相关信息
-        factory.setHost(Host);
-        factory.setPort(Port);
-        factory.setVirtualHost(VirtualHost);
-        factory.setUsername(Username);
-        factory.setPassword(Password);
         //创建一个新的连接
         Connection connection = factory.newConnection();
-        logger.info("连接mq:"+Host+","+VirtualHost+","+TYPE+","+TYPE_VALUE);
         //exchange的类型包括:direct, topic, headers and fanout,我们本例子主要关注的是fanout
         //fanout类型是指向所有的队列发送消息
         //以下是创建一个fanout类型的exchange,取名logs
         Channel channel = connection.createChannel();
-        channel.exchangeDeclare(TYPE_VALUE, BuiltinExchangeType.FANOUT);
 
-        String message = "{data}";
+        try {
 
-        //1.在上个"hello world"例子中,我们用的是channel.basicPublish("", "hello", null, message.getBytes());
-        //这里用了默认的exchanges,一个空字符串 "",在basicPublish这个方法中,第一个参数即是exchange的名称
-        //2.准备向我们命名的exchange发送消息啦
-        channel.basicPublish(TYPE_VALUE, "", null, message.getBytes("UTF-8"));
-        logger.info("推送消息:"+message);
-        System.out.println("producer send:" + message);
+            //设置RabbitMQ相关信息
+            factory.setHost(this.getHost());
+            if(this.getPort()!=null){
+                factory.setPort(Integer.parseInt(this.getPort()));
+            }
+            if(this.getVirtualHost()!=null){
+                factory.setVirtualHost(this.getVirtualHost());
+            }
 
-        channel.close();
-        connection.close();
+            factory.setUsername(this.getUsername());
+            factory.setPassword(this.getPassword());
 
+            logger.info("连接mq:"+this.getHost()+","+this.getVirtualHost()+","+this.getExchangeType()+","+this.getExchangeName());
+
+            if (this.getExchangeType().trim()!=null){
+                //        channel.exchangeDeclare(this.getExchangeName(), BuiltinExchangeType.FANOUT);
+                channel.exchangeDeclare(this.getExchangeName(), this.getExchangeType().toLowerCase());
+            }else {
+                channel.queueDeclare(this.getExchangeName(), false, false, false, null);
+            }
+
+
+            String message = this.getMessage();
+
+            //1.在上个"hello world"例子中,我们用的是channel.basicPublish("", "hello", null, message.getBytes());
+            //这里用了默认的exchanges,一个空字符串 "",在basicPublish这个方法中,第一个参数即是exchange的名称
+            //2.准备向我们命名的exchange发送消息啦
+            channel.basicPublish(this.getExchangeName(), "", null, message.getBytes("UTF-8"));
+            logger.info("推送消息:"+ message);
+            System.out.println("producer send:" + message);
+
+
+        }catch (Exception e){
+            channel.close();
+            connection.close();
+            return false;
+
+        }finally {
+            channel.close();
+            connection.close();
+        }
         return true;
     }
 
 
+    public  String getHost() {
+        return getPropertyAsString(Host);
+    }
+
+    public  void setHost(String host) {
+        setProperty(Host,host);
+    }
+
+    public String getPort() {
+        return getPropertyAsString(Port);
+    }
+
+    public  void setPort(String port) {
+        setProperty(Port,port);
+    }
+
+    public  String getVirtualHost() {
+        return getPropertyAsString(VirtualHost);
+    }
+
+    public  void setVirtualHost(String virtualHost) {
+        setProperty(VirtualHost,virtualHost);
+    }
+
+    public  String getUsername() {
+        return getPropertyAsString(Username);
+    }
+
+    public  void setUsername(String username) {
+        setProperty(Username,username);
+    }
+
+    public String getPassword() {
+        return getPropertyAsString(Password);
+    }
+
+    public void setPassword(String password) {
+        setProperty(Password, password);
+    }
+
+    public  String getExchangeType() {
+        return getPropertyAsString(ExchangeType);
+    }
+
+    public void setExchangeType(String exchangeType) {
+        setProperty(ExchangeType, exchangeType);
+    }
+
+    public  String getExchangeName() {
+        return getPropertyAsString(ExchangeName);
+    }
+
+    public void setExchangeName(String exchangeName) {
+        setProperty(ExchangeName, exchangeName);
+    }
+
     public String getMessage() {
-        return getPropertyAsString(KAFKA_MESSAGE);
+        return getPropertyAsString(Message);
     }
 
     public void setMessage(String message) {
-        setProperty(KAFKA_MESSAGE, message);
-    }
-
-    public String getMessageSerializer() {
-        return getPropertyAsString(KAFKA_MESSAGE_SERIALIZER);
-    }
-
-
-    @Override
-    public void testStarted() {
-
+        setProperty(Message, message);
     }
 
     @Override
-    public void testStarted(String s) {
-
-    }
+    public void testStarted() {}
 
     @Override
-    public void testEnded() {
-
-    }
+    public void testStarted(String s) {}
 
     @Override
-    public void testEnded(String s) {
+    public void testEnded() {}
 
-    }
+    @Override
+    public void testEnded(String s) {}
 
 
 }
