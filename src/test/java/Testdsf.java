da5304753646572e414af0e6a479af833a2ab968
linweili <295287765@qq.com>
2018-07-10 18:06:38 +0800

    222

--- src/test/java/Testdsf.java
+++ src/test/java/Testdsf.java
@@ -1,4 +1,5 @@
 import com.google.protobuf.Internal;
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -17,31 +18,50 @@ import java.util.concurrent.TimeoutException;
 public class Testdsf {
 
     private static final Logger logger = LoggerFactory.getLogger(Testdsf.class);
-    private static final String  QUEUE_NAME = "jmeter_queue";
+    private static final String Host = "node.td-k8s.com";
+    private static final String Port = "5672";
+    private static final String VirtualHost = "/globalBizEvent";
+    private static final String Username = "admin";
+    private static final String Password = "admin";
+    private static final String ExchangeType = "fanout";
+    private static final String ExchangeName = "globalBizEventWeExchage";
+    private static final String Message = "message";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         //创建连接工厂
         ConnectionFactory factory = new ConnectionFactory();
-        //设置RabbitMQ相关信息
-        factory.setHost("node.td-k8s.com");
-        factory.setUsername("admin");
-        factory.setPassword("admin");
-        factory.setPort(5672);
         //创建一个新的连接
+        factory.setHost(Host);
+        factory.setPort(Integer.parseInt(Port));
+        factory.setVirtualHost(VirtualHost);
+        factory.setUsername(Username);
+
+        factory.setPassword(Password);
         Connection connection = factory.newConnection();
-        logger.info("连接mq:"+"node.td-k8s.com");
-        //创建一个通道
+        //exchange的类型包括:direct, topic, headers and fanout,我们本例子主要关注的是fanout
+        //fanout类型是指向所有的队列发送消息
+        //以下是创建一个fanout类型的exchange,取名logs
         Channel channel = connection.createChannel();
-        //声明一个队列
-        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
-        //发送消息到队列中
-        String message = "Hello MQ";
-        channel.basicPublish("",QUEUE_NAME,null,message.getBytes("UTF-8"));
-        logger.info("推送消息:"+message);
-        System.out.println("producer send:" + message);
-        //关闭通道及连接
+
+
+
+        channel.exchangeDeclare(ExchangeName, ExchangeType.toLowerCase());
+
+        String message = "{\n" +
+                "\t\"signInfo\": {\n" +
+                "\t\t\"userId\": \"BC397BD2-867F-475D-B5D0-A17B64DC2B1E\",\n" +
+                "\t\t\"signDate\": \"2018-05-18\",\n" +
+                "\t\t\"type\": 1,\n" +
+                "\t\t\"continuousSignedDays\": 5\n" +
+                "\t}\n" +
+                "}";
+
+        //1.在上个"hello world"例子中,我们用的是channel.basicPublish("", "hello", null, message.getBytes());
+        //这里用了默认的exchanges,一个空字符串 "",在basicPublish这个方法中,第一个参数即是exchange的名称
+        //2.准备向我们命名的exchange发送消息啦
+        channel.basicPublish(ExchangeName, "", null, message.getBytes("UTF-8"));
         channel.close();
         connection.close();
-        logger.info("mq已断开:"+message);
+        }
     }
-}
+
