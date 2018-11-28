d190b69203480b8f4bf9637004f053d1d01480ef
linweili <295287765@qq.com>
2018-11-28 21:20:37 +0800

    md5 size

--- src/main/java/td/functions/MD5.java
+++ src/main/java/td/functions/MD5.java
@@ -1,6 +1,7 @@
 package td.functions;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.jmeter.engine.util.CompoundVariable;
 import org.apache.jmeter.functions.AbstractFunction;
 import org.apache.jmeter.functions.InvalidVariableException;
@@ -27,10 +28,12 @@ public class MD5 extends AbstractFunction {
 
     static {
         desc.add(JMeterUtils.getResString("param"));
+        desc.add(JMeterUtils.getResString("size"));
         desc.add(JMeterUtils.getResString("function_name_paropt")); //$NON-NLS-1$
     }
 
     private CompoundVariable param;
+    private CompoundVariable size;
     private CompoundVariable varName;
 
 
@@ -43,17 +46,16 @@ public class MD5 extends AbstractFunction {
     public String execute(SampleResult previousResult, Sampler currentSampler) throws InvalidVariableException {
 
             String max = param.execute().trim();
+            String sizes = size.execute().trim();
+        System.out.println("ff" + sizes + "dd");
             if(max==null || max ==""){
                 return null;
             }else {
                 try {
-                    System.out.println(max);
                     MessageDigest md = MessageDigest.getInstance("MD5");
                     md.update(max.getBytes());
                     byte b[] = md.digest();
-
                     int i;
-
                     StringBuffer buf = new StringBuffer("");
                     for (int offset = 0; offset < b.length; offset++) {
                         i = b[offset];
@@ -65,17 +67,27 @@ public class MD5 extends AbstractFunction {
                         }
                         buf.append(Integer.toHexString(i));
                     }
-                    //32位加密
-//                    System.out.println("md5:"+buf.toString().toLowerCase());
+
+                    String md5res = "";
+                    if(sizes.equals("big") || "".equals(sizes)){
+                        md5res = buf.toString().toUpperCase();
+                    }else if (sizes.equals("small")){
+                        md5res = buf.toString().toLowerCase();
+                    }else {
+                        md5res = buf.toString().toUpperCase();
+                    }
+
                     if (varName != null) {
                         JMeterVariables vars = getVariables();
                         final String varTrim = varName.execute().trim();
                         if (vars != null && varTrim.length() > 0){// vars will be null on TestPlan
-                            vars.put(varTrim, buf.toString().toLowerCase());
+                            vars.put(varTrim, md5res);
                         }
                     }
 
-                    return buf.toString().toLowerCase();
+                    return md5res;
+                    //32位加密
+//                    System.out.println("md5:"+buf.toString().toLowerCase());
                     // 16位的加密
                     //return buf.toString().substring(8, 24);
                 } catch (NoSuchAlgorithmException e) {
@@ -94,11 +106,12 @@ public class MD5 extends AbstractFunction {
 
     @Override
     public void setParameters(Collection<CompoundVariable> parameters) throws InvalidVariableException {
-        checkParameterCount(parameters, 1, 2);
+        checkParameterCount(parameters, 1, 3);
         Object[] values = parameters.toArray();
         param = (CompoundVariable)values[0];
-        if (values.length>1){
-            varName = (CompoundVariable) values[1];
+        size = (CompoundVariable)values[1];
+        if (values.length > 2){
+            varName = (CompoundVariable) values[2];
         } else {
             varName = null;
         }
